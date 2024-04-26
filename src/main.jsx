8ef6217b63623bc2690c43ea23ccfb57f85aa6ea
rdwrds <r.edwards1492@gmail.com>
2024-04-26 16:13:40 -0500

    grid changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Builds from './Builds.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className='bg-gradient-to-b from-blue-7 to-blue-6 h-full w-full'>
+    <Builds />
+    </div>
   </React.StrictMode>,
 )
